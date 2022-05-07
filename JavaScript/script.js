d9da8635b46cb4ee82f8453c3b3dd4e49fdf012c
Angad Kumar Mandal <76276545+ANGADMADHEPURA@users.noreply.github.com>
2022-05-07 16:19:13 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,25 @@
+
+
+const d = new Date();
+alert("Today's date is " + d);
+
+console.log("This Website Design By BALVINDER SINGH & ANGAD MANDAL");
+console.log("Web_development");
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+
+  function w3r(){
+    alert('Welcome all the best USER Go to the Website');
+    window.alert('Click OK');
+    } 
+
+    function msg(){
+      alert("Hello Friends");
+    }
